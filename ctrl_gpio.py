d493ca5c14f262e48e10d756e5d28c91429c206d
fah <farhan.abuhazan@gmail.com>
2019-09-19 14:43:40 +0700

    gpio control WIP

--- ctrl_gpio.py
+++ ctrl_gpio.py
@@ -1,36 +1,47 @@
-from subprocess import call
-from time import sleep
-import os 
+import os
 import threading
 from time import sleep
-import time
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-# Note that you have to specify path to script
-
+from subprocess import call
 from gpiozero import Button, LED
 from signal import pause
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+buttonPin = 5
+ledPin = 6
 
-button = Button(5, hold_time=5)
-led = LED(4)
+button = Button(buttonPin, hold_time=5)
+led = LED(ledPin)
 
-file_path = dir_path + "/access-point.js"
-print file_path
+startApFile = f'{dir_path}/src/scripts/start_ap.sh'
+print("[Info] Reset to ap mode service started...")
 
 def led_blink():
-    while(True):
+    for num in range(5):
         led.toggle()
-        sleep(0.2)
-        
+        sleep(0.1)
+        led.toggle()
+        sleep(0.1)
+        led.toggle()
+        sleep(0.1)
+        led.toggle()
+        sleep(0.7)
+
 def exec_wifi():
+    print("[Info] reset wifi to access point mode.")
     t = threading.Thread(target=led_blink)
     t.start()
-    call(["node", file_path]) 
+    call(["sudo", "sh", startApFile])
+
+def onButtonPressed():
+    print('[Info] Button is pressed.')
+    led.on()
+
+def onButtonRelease():
+    print('[Info] Button is pressed.')
+    led.off()
 
 button.when_held = exec_wifi
-button.when_pressed = led.on
-button.when_released = led.off
-
+button.when_pressed = onButtonPressed
+button.when_released = onButtonRelease
 
 pause()
