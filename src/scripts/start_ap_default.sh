f8ea02bdcd1748f607601159e901c0f32d909b76
Fah <farhan.abuhazan@gmail.com>
2020-11-02 19:52:49 +0700

    Fixed bug, Reset Wifi

--- src/scripts/start_ap_default.sh
+++ src/scripts/start_ap_default.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 hostapd_pid=/run/hostapd.pid
 BASE_DIR=$(dirname "$0")
-ssid=$($"$BASE_DIR/get_ssid.sh")
+ssid=$("$BASE_DIR/get_ssid.sh")
 
 sudo cp $BASE_DIR/preconfig/ap_dhcpcd.conf /etc/dhcpcd.conf
 sudo cp $BASE_DIR/preconfig/ap_hostapd.conf /etc/hostapd/hostapd.conf
